0fb04e883e3bb4c726262cc1d2a7010552c4a8f1
arandomwebdev <thegoatsk03@gmail.com>
2025-11-23 10:07:08 -0500

    Refactor degtrig.py to keep only degSin and degCos
    Removed unused functions and retained degSin and degCos.

--- degtrig.py
+++ degtrig.py
@@ -7,15 +7,3 @@ def degSin(number):
 def degCos(number):
     # returns the cosine of the number in degrees
     return cos(number * pi / 180)
-
-def degTan(number):
-    # returns the tangent of the number in degrees
-    return tan(number * pi / 180)
-
-def degArctan(number):
-    # returns the arc tangent of the number in degrees
-    return atan(number * pi / 180)
-
-def degArctan2(number1, number2):
-    # returns the arc tangent of the number in degrees
-    return atan2(number1 * pi / 180, number2 * pi / 180)
